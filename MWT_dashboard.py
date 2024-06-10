446b1bc2bf7181a5d68ee5bf2dcb7b9ef5fb5872
RankinLabUBC <rankin.lab@gmail.com>
2024-06-10 11:49:09 -0700

    Update MWT_dashboard.py

--- MWT_dashboard.py
+++ MWT_dashboard.py
@@ -23,9 +23,9 @@ def read(table):
     result = pd.read_sql_query(f"SELECT * FROM {table}", conn)
     return result
 
-conn = sqlite3.connect('/Users/Joseph/Desktop/NRSC510B/mwt_data.db')
+# conn = sqlite3.connect('/Users/Joseph/Desktop/NRSC510B/mwt_data.db')
 # conn = sqlite3.connect('/Users/lavanya/Downloads/MWT_Dashboard-main/Test/mwt_data.db')
-# conn = sqlite3.connect('/Users/rankinlab/Desktop/MWT_Data_App/mwt_data.db')
+conn = sqlite3.connect('/Users/rankinlab/Desktop/MWT_Data_App/mwt_data.db')
 
 # Read data from SQLite database
 tap_output = read('tap_response_data')
