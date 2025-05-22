121f69252092d9593c404c0f622c177b5dacd328
gurmehak <gurmehak99@gmail.com>
2025-05-22 00:16:56 -0700

    set up app.py

---
+++ mwt_dashboard/app.py
@@ -0,0 +1,16 @@
+from pages import home, gene, allele, custom_gene, custom_allele, help, citations
+
+if page == pages[0]:
+    home.render(data)
+elif page == pages[1]:
+    gene.render(data)
+elif page == pages[2]:
+    allele.render(data)
+elif page == pages[3]:
+    custom_gene.render(data)
+elif page == pages[4]:
+    custom_allele.render(data)
+elif page == pages[5]:
+    help.render(data)
+elif page == pages[6]:
+    citations.render(data)
