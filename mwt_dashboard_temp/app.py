81ab680f8ea3272b3b210955b1eaf4751163d27b
gurmehak <gurmehak99@gmail.com>
2025-05-27 13:50:55 -0700

    update app.py for 3 pages

--- mwt_dashboard_temp/app.py
+++ mwt_dashboard_temp/app.py
@@ -1,20 +1,29 @@
-from pages import home, gene, allele, custom_gene, custom_allele, help, citations
+import streamlit as st
+from config import pages
+from utils.data_loader import fetch_data
+from utils.preprocess import select_datasets
+from pages import home, gene, allele  # import other pages as needed
+from utils.auth import check_password
 
+# if not check_password():
+#     st.stop()
+
+st.set_page_config(page_title="MWT Dashboard", layout="wide")
+st.title("MWT Data Dashboard - Rankin Lab @ UBC")
+
+# Select a page
+page = st.sidebar.radio("Select a page", pages)
+
+# Load and filter data
 data = fetch_data()
+select_datasets(data)
 
-data = select_datasets(data) 
-
+# Route pages
 if page == pages[0]:
     home.render(data)
 elif page == pages[1]:
     gene.render(data)
 elif page == pages[2]:
     allele.render(data)
-elif page == pages[3]:
-    custom_gene.render(data)
-elif page == pages[4]:
-    custom_allele.render(data)
-elif page == pages[5]:
-    help.render(data)
-elif page == pages[6]:
-    citations.render(data)
+# Add more pages as needed
+
