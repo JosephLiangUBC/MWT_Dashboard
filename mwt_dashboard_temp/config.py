ae025ba8b177c4eb3b27f02e2a9b8ea167169e33
gurmehak <gurmehak99@gmail.com>
2025-05-27 13:52:50 -0700

    add dahsboard configuration files

---
+++ mwt_dashboard_temp/config.py
@@ -0,0 +1,34 @@
+# config.py
+
+# This controls the page names shown in the sidebar
+pages = [
+    "Home - Data at a Glance",
+    "Gene-specific Data",
+    "Allele-specific Data",
+    "Custom Gene Selection",
+    "Custom Allele Selection",
+    "Help & Documentation",
+    "Citations"
+]
+
+# Plotly config for all download buttons
+plotly_config = {
+    'toImageButtonOptions': {
+        'format': 'png',
+        'filename': 'plot',
+        'height': None,
+        'width': None,
+        'scale': 3
+    }
+}
+
+# Optional: your color palette used in barplots
+metric_palette = [
+    "k", "k", "k",
+    "darkgray", "darkgray", "darkgray", "darkgray", "darkgray", "darkgray", "darkgray", "darkgrey", "darkgray",
+    "lightsteelblue", "lightsteelblue", "lightsteelblue",
+    "powderblue", "powderblue", "powderblue",
+    "cadetblue", "cadetblue", "cadetblue",
+    "thistle", "thistle", "thistle",
+    "slateblue", "slateblue", "slateblue"
+]
