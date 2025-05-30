44e7c7897c1dc13190576262f3f7a389535815a3
gurmehak <gurmehak99@gmail.com>
2025-05-30 08:20:50 -0700

    add app.py and update data_loader for custom gene and allele

--- mwt_dashboard_temp/app.py
+++ mwt_dashboard_temp/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from config import pages, metric_palette, config
 from utils.data_loader import fetch_data
 from utils.preprocess import select_datasets
-from pages import home, gene, allele, help, citations
+from pages import home, gene, allele, help, citations, custom_gene, custom_allele
 from utils.auth import check_password
 
 # if not check_password():
@@ -28,6 +28,10 @@ elif page == pages[1]:
     gene.render(data)
 elif page == pages[2]:
     allele.render(data)
+elif page == pages[3]:
+    custom_gene.render(data)
+elif page == pages[4]:
+    custom_allele.render(data)
 elif page == pages[5]:
     help.render()
 elif page == pages[6]:
