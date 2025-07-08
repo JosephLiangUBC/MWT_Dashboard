4f3f86e40706263d89f1578c419ba55ae92328ef
gurmehak <gurmehak99@gmail.com>
2025-07-07 17:31:42 -0700

    add visualisation for psa

--- mwt_dashboard_temp/app_pages/psa.py
+++ mwt_dashboard_temp/app_pages/psa.py
@@ -12,17 +12,100 @@ from config import config
 def render(data):
     st.header("Post Stimulus Arousal Data")
 
-    psa_data = data["psa_output"]
+    psa_df = data["psa_output"]
 
-    st.subheader("Raw Data Preview")
-    st.dataframe(psa_data)
+    # Filter boxes fro metric and summary
+    metric_option = st.selectbox("Select metric", ['Instantaneous Speed', 'Bias', 'Angular Speed', 'Kink', 'Crab', 'Aspect Ratio', 'Curve'])
+    summary_option = st.selectbox("Select summary", ['Initial', 'Recovery', 'Peak', 'Initial_to_peak', 'Peak_to_recovery', 'Average'])
 
-    with st.expander("Show column names and data types"):
-        st.write(psa_data.dtypes)
+    # Filter box for gene
+    # gene_option = st.selectbox("Select a gene", psa_df["Gene"].unique())
+    # df_filtered = psa_df[psa_df["Gene"] == gene_option]
 
-    st.download_button(
-        label="Download CSV",
-        data=convert_df(psa_data),
-        file_name="psa_data.csv",
-        mime="text/csv"
+    # sort by gene
+    gene_order = (
+    psa_df.groupby("Gene")[f"PSA {summary_option} {metric_option}"]
+    .mean()
+    .sort_values()
+    .index
     )
+
+    # Plot
+    st.subheader(f"Full Post-Stimulus Arousal Response — {metric_option}")
+    
+    fig, ax = plt.subplots(figsize=(15, 5))
+    sns.set_context("poster")
+    sns.barplot(
+        x="Gene",
+        y=f"PSA {summary_option} {metric_option}",
+        data=psa_df,
+        hue = "Gene",
+        order = gene_order,
+        estimator='mean',
+        errorbar=("ci", 95),
+        ax=ax,
+        orient="v",
+        legend=False
+    )
+    # sns.stripplot(
+    #     x="Tap_num",
+    #     y=f"{metric_option}",
+    #     data=df_filtered,
+    #     size=6,
+    #     color="k",
+    #     ax=ax
+    # )
+    ax.set_title(f"Post Stimulus Arousal {summary_option} {metric_option} - all genes", fontsize=14)
+    ax.set_ylabel(f"{summary_option} {metric_option}", fontsize=12)
+    ax.set_xlabel("Gene", fontsize=12)
+    plt.xticks(rotation=90, fontsize=10)
+    plt.yticks(fontsize=12)
+    # ax.set_ylim(top=0.33)
+    st.pyplot(fig)
+
+
+
+
+
+
+
+    # Summary charts?
+    # st.subheader(f"{metric_option} by Summary Type")
+
+    # summaries = ['Initial', 'Recovery', 'Peak', 'Initial_to_peak', 'Peak_to_recovery', 'Average']
+    # col1, col2 = st.columns(2)
+
+    # for i, summary in enumerate(summaries):
+    #     col = col1 if i % 2 == 0 else col2
+
+    #     # Build the correct column name
+    #     column_name = f"PSA {summary} {metric_option}"
+    #     if column_name not in df_filtered.columns:
+    #         col.warning(f"{column_name} not found in data.")
+    #         continue
+
+    #     fig, ax = plt.subplots(figsize=(7, 5))
+    #     sns.set_context("talk")
+    #     sns.barplot(
+    #         x="Screen", y=column_name,
+    #         data=df_filtered,
+    #         hue="Screen",
+    #         ax=ax,
+    #         estimator='mean',
+    #         errorbar=("ci", 95),
+    #         palette="muted",
+    #         legend=False
+    #     )
+    #     sns.stripplot(
+    #         x="Screen", y=column_name,
+    #         data=df_filtered,
+    #         size=5,
+    #         color="k",
+    #         ax=ax
+    #     )
+    #     ax.set_title(f"{summary} {metric_option}")
+    #     ax.set_xlabel("")
+    #     ax.set_ylabel(metric_option)
+    #     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+
+    #     col.pyplot(fig)
--- mwt_dashboard_temp/utils/preprocess.py
+++ mwt_dashboard_temp/utils/preprocess.py
@@ -35,6 +35,8 @@ def select_datasets(data):
 
     data["tap_output"] = data["tap_output"][data["tap_output"]["Screen"].isin(datasets)].replace(["N2_N2", "N2_XJ1"], "N2")
 
+    data["psa_output"] = data["psa_output"][data["psa_output"]["Screen"].isin(datasets)].replace(["N2_N2", "N2_XJ1"], "N2")
+
     data["tap_tstat_allele"] = (
         data["tap_tstat_allele"][data["tap_tstat_allele"]["Screen"].isin(datasets)]
         .dropna(subset=[col for col in dropna_features if col in data["tap_tstat_allele"].columns])
@@ -49,10 +51,12 @@ def select_datasets(data):
         .replace(["N2_N2", "N2_XJ1"], "N2")
     )
 
-
     data["gene_profile_data"] = data["gene_profile_data"][data["gene_profile_data"]["Screen"].isin(datasets)].replace(["N2_N2", "N2_XJ1"], "N2")
+    
     data["allele_profile_data"] = data["allele_profile_data"][data["allele_profile_data"]["Screen"].isin(datasets)].replace(["N2_N2", "N2_XJ1"], "N2")
+    
     data["gene_MSD"] = data["gene_MSD"][data["gene_MSD"]["Screen"].isin(datasets)].replace(["N2_N2", "N2_XJ1"], "N2")
+    
     data["allele_MSD"] = data["allele_MSD"][data["allele_MSD"]["Screen"].isin(datasets)].replace(["N2_N2", "N2_XJ1"], "N2")
 
     return data
