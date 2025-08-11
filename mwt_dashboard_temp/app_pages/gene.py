a3939b323bc506fddd9419d4249ac8b931c9ee87
Joseph Liang <joseph90003@gmail.com>
2025-08-11 00:01:56 -0700

    fix typo (speed vs instantaneous speed)

--- mwt_dashboard_temp/app_pages/gene.py
+++ mwt_dashboard_temp/app_pages/gene.py
@@ -221,7 +221,7 @@ def render(data):
     with col7:
 
         metrics = [ "Probability", "Duration", "Speed",
-                   "PSA Instantaneous Speed", "PSA Interval Speed",
+                   "PSA Speed", "PSA Interval Speed",
                     "PSA Bias", "PSA Kink", "PSA Crab",
                     "PSA Aspect Ratio", "PSA Curve"]
         
