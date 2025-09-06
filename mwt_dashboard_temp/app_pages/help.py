486d2c4f29b0807225f573f2b39702dd67148b60
Joseph Liang <joseph90003@gmail.com>
2025-09-05 18:40:46 -0700

    Update help.py documentation

--- mwt_dashboard_temp/app_pages/help.py
+++ mwt_dashboard_temp/app_pages/help.py
@@ -5,23 +5,21 @@ def render(data=None):
     ## Home - Getting Started
 
     ### About the Data Dashboard:
-    Since the inception of the [Multi-Worm Tracker (MWT)](https://doi.org/10.1038/nmeth.1625), the Rankin Lab has collected vast amounts of data from a large number of strains across several experimental screens. 
-    This dashboard, written by Joseph Liang (PhD Candidate), consolidates MWT data collected over the years following the standard 10-second ISI (30 stimuli at 10-second inter-stimulus intervals) habituation protocols.
-
-    Although the MWT is powerful in capturing rich multi-phenotype data from large populations of living animals simultaneously, the raw output is difficult to access for users unfamiliar with its inner workings.
-    This tool addresses that barrier by processing and visualizing the appropriate datasets through an easy-to-use dashboard.
+                         
+    #### Since the inception of the [Multi-Worm Tracker (MWT)](https://doi.org/10.1038/nmeth.1625), the Rankin Lab has collected vast amounts of data from a large number of strains across several experimental screens. Although the MWT is powerful in capturing rich multi-phenotype data from large populations of living animals simultaneously, the raw output is difficult to access for users unfamiliar with its inner workings. This tool addresses that barrier by processing and visualizing the appropriate datasets through an easy-to-use dashboard.
+    #### This dashboard, written by Joseph Liang (PhD Candidate), consolidates MWT data collected over the years following the standard 10-second ISI (30 stimuli at 10-second inter-stimulus intervals) habituation protocols. This dashboard provides access to a consolidated database containing genotype-phenotype relationships for 944 (and counting) genotypes, with easily accessible visualization tools to compare between strains, between experiments.
 
     ### The Data
-    Data in this dashboard comes from a variety of experiments conducted by students of the Rankin Lab. These studies examine *C. elegans* strains with (mainly loss-of-function) mutations in genes of interest.
+    #### Data in this dashboard comes from a variety of experiments conducted by students of the Rankin Lab. These studies examine *C. elegans* strains with (mainly loss-of-function) mutations in genes of interest.
 
-    All experiments use the 10s-ISI tap-habituation protocol. Heatmaps and phenomic profiles are calculated by comparing mutant strains to N2 wild-type controls run on the same days.
+    #### All experiments use the 10s-ISI tap-habituation protocol. Heatmaps and phenomic profiles are calculated by comparing mutant strains to N2 wild-type controls run on the same days. Habituation curves are plotted with comparison to N2 replicates run in the same experiment(s).
 
     ### Getting Started
-    Navigate between pages using the sidebar. Gene- and allele-specific pages display summarized data for each gene or allele.
-    To make cross-gene comparisons, use the “Custom Gene/Allele Selection” pages.
+    #### Navigate between pages using the sidebar. Gene- and allele-specific pages display summarized data for each gene or allele.
+    #### To make cross-gene comparisons, use the “Custom Gene/Allele Selection” pages.
 
-    You can download all plots and datasets directly from the interface. Don’t like our visualizations? Export the data and create your own using your favorite tools.
+    #### You can download all plots and datasets directly from the interface. Don’t like our visualizations? Export the data and create your own using your favorite tools.
 
     ### Contact
-    For bugs, issues, or feature requests, please contact Joseph Liang at **joseph.liang@psych.ubc.ca**.
+    #### For bugs, issues, or feature requests, please contact Joseph Liang at **joseph.liang@psych.ubc.ca**.
     """)
