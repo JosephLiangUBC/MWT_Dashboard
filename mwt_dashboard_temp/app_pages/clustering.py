404c45b0592687d0c4c7e80efea3695b7936daa0
gurmehak <gurmehak99@gmail.com>
2025-10-12 18:40:10 -0700

    upload final clustering page without n_clusters filter

--- mwt_dashboard_temp/app_pages/clustering.py
+++ mwt_dashboard_temp/app_pages/clustering.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from utils.helpers import convert_df, read
 from config import config
+import psycopg
 
 # clustering specific imports
 from sqlalchemy import create_engine
@@ -18,14 +19,26 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.manifold import TSNE
+import plotly.express as px
 
+
+    
 def render(data):
-    st.header("Clustering data by genes")
 
+    st.header("Clustering Genes")
+
+    # n_clusters = st.selectbox(
+    #     "Select number of clusters",
+    #     (np.nu)
+    # )
     df = data['tap_tstat_data']
+
+    gene_names = df['Gene'].values
 
     df = df[~df.isin([np.inf, -np.inf]).any(axis=1)]
 
+    # Numerical Columns 
     numeric_cols = df.select_dtypes(include='number').columns
     si = SimpleImputer(strategy="constant", fill_value=-9999)
     scaler = StandardScaler()
@@ -36,6 +49,8 @@ def render(data):
     ohe = OneHotEncoder(handle_unknown="ignore", sparse_output=False, dtype = int)
     categorical_transformer = make_pipeline(ohe)
 
+
+
     # Apply transformations
     preprocessor = make_column_transformer(
         (numerical_transformer, numeric_cols),
@@ -45,42 +60,33 @@ def render(data):
 
     X = preprocessor.fit_transform(df)
 
-    # pca = PCA()
-    # pca.fit(X)
-
     # PCA + KMeans
     pca = PCA(n_components=99)
-    pca_df = pca.fit_transform(X)
+    pca_df = pd.DataFrame(pca.fit_transform(X))
     kmeans = KMeans(n_clusters=6, n_init='auto', random_state=100)
     labels = kmeans.fit_predict(pca_df)
-    
-    df['Cluster'] = labels
 
+    # tSNE to visualise multidimensional data in 2D
+    tsne = TSNE(n_components=2, random_state=100, perplexity=30)
+    tsne_df = pd.DataFrame(tsne.fit_transform(pca_df), columns=['tSNE1', 'tSNE2'])
+    tsne_df['Cluster'] = labels
+    tsne_df['Gene'] = gene_names
 
-    # Plot
-    pca_df_vis = PCA(n_components=2, random_state=100) # n_components=2 for 2D plot
-    labels_vis = pca_df_vis.fit_transform(pca_df)                     
-    centers_vis = pca_df_vis.transform(kmeans.cluster_centers_)
-    method = "PCA(2)"
+    # internal tests 
+    # st.write(df.head(1))
+    # st.write(pca_df.head(1))
+    # st.write(tsne_df.head(1))
+    # st.write(tsne_df['Gene'].isnull().sum())
 
-    plt.figure(figsize=(8,6))
-    plt.scatter(labels_vis[:, 0], labels_vis[:, 1], c=labels, cmap="tab10", alpha=0.7, s=15, linewidths=0)
-    plt.scatter(centers_vis[:, 0], centers_vis[:, 1], marker="X", s=10, c="black", label="Centroid")
-    plt.title(f"Clusters (assigned in 100D)")
-    # plt.xlabel("PCA 1"); plt.ylabel("PCA 2")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-
-
-
-
-    # # ------ fix
-    # st.write("Columns in df:", df.columns.tolist())
-    # st.write("Numeric cols:", numeric_cols)
-    # st.write("Categorical cols:", categorical_cols)
-    # st.write("X shape:", X.shape)
-    # st.write("X type:", type(X))
-    # st.write("First row:", X[0])
+    # Plot 
+    fig = px.scatter(
+        tsne_df,
+        x="tSNE1",
+        y="tSNE2",
+        color="Cluster",
+        hover_data={"Gene": True, "Cluster": True, "tSNE1":False, "tSNE2":False},
+        title="t-SNE Visualization of Gene Clusters (KMeans labels)",
+        color_continuous_scale="Viridis"
+    )
+    fig.update_traces(marker=dict(size=6, opacity=0.8, line=dict(width=0)))
+    st.plotly_chart(fig, use_container_width=True)
